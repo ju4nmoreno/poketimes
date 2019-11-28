3fad6366cd93ba5f17a4d8c1ca6de1a7a9b12008
juan moreno <jcmm.info@gmail.com>
2019-11-28 14:28:11 -0500

    add build projecto to folder docs

---
+++ docs/precache-manifest.4a108e8d5759b02b041c22259a34086e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53de3a9ed2bc605cb7b0089b8f359b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e4b9d381102f5fd7f1a",
+    "url": "/static/css/main.8587d202.chunk.css"
+  },
+  {
+    "revision": "4a93adcf34a95e4f231b",
+    "url": "/static/js/2.bdc8cc70.chunk.js"
+  },
+  {
+    "revision": "5e4b9d381102f5fd7f1a",
+    "url": "/static/js/main.1e90bf5c.chunk.js"
+  },
+  {
+    "revision": "a7cb9a2bbf2d1e60c1e2",
+    "url": "/static/js/runtime-main.05cd91fe.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/static/media/pokeball.f10bf078.png"
+  }
+]);
